e062921a7ba32b813560c83093578c6f99a326a8
JW <info@lh-s.net>
2023-10-21 22:22:40 +0200

    add a spinner

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,10 +1,10 @@
 import {NextRequest, NextResponse} from "next/server";
 import prisma from "@/prisma/client"
-import {validationSchemas} from "@/app/validationSchemas";
+import {createIssueSchema} from "@/app/validationSchemas";
 
 export async function POST(request: NextRequest) {
     const body = await request.json()
-    const validation = validationSchemas.safeParse(body)
+    const validation = createIssueSchema.safeParse(body)
     if (!validation.success)
         // return NextResponse.json(validation.error.errors, {status: 400})
         return NextResponse.json(validation.error.format(), {status: 400})
