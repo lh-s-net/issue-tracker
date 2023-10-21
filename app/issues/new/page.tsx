ab7c70db89dbefef09cf3de4ac163828c3c03012
JW <info@lh-s.net>
2023-10-21 17:24:29 +0200

    add a markdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
-import {Button, TextArea, TextField} from "@radix-ui/themes";
+import {Button, TextField} from "@radix-ui/themes";
 
 const NewIssuePage = () => {
     return (
