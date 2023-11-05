dd9befd9366e938e3c936d1d37abd4ac28dd0b2a
JW <info@lh-s.net>
2023-11-05 17:29:35 +0100

    enhance the api to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,32 +1,50 @@
+import authOptions from "@/app/auth/authOptions";
 import {NextRequest, NextResponse} from "next/server";
-import {issueSchema} from "@/app/validationSchemas";
+import {patchIssueSchema} from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
+import {getServerSession} from "next-auth";
 
 // PATCH
 export async function PATCH(
     request: NextRequest,
     {params}: { params: { id: string } }) {
+
+    const  session = await getServerSession(authOptions)
+    if (!session) return NextResponse.json({}, {status: 401})
+
     const body = await request.json()
-    const validation = issueSchema.safeParse(body)
+    const validation = patchIssueSchema.safeParse(body)
     if (!validation.success)
         return NextResponse.json(validation.error.format(), {status: 400})
+
+    const {title, description, assignedToUserId} = body
+
+    if (assignedToUserId) {
+        const user = await prisma.user.findUnique({where: {id: body.assignedToUserId}})
+        if (!user)
+            return NextResponse.json({error: "Invalid assigned user!"}, {status: 400})
+    }
 
     const issue = await prisma.issue.findUnique({
         where: {id: parseInt(params.id)}
     })
 
     if (!issue)
-        return NextResponse.json({error: "Invalid issue"}, {status: 404})
+        return NextResponse.json(
+            {error: "Invalid issue"},
+            {status: 404}
+        )
 
     const updatedIssue = await prisma.issue.update({
         where: {id: issue.id},
         data: {
-            title: body.title,
-            description: body.description
+            title,
+            description,
+            assignedToUserId
         }
     })
 
-    return NextResponse.json(updatedIssue, {status: 200})
+    return NextResponse.json(updatedIssue)
 
 }
 
--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -4,5 +4,24 @@ export const issueSchema = z.object({
     // title: z.string().min(1).max(255),
     title: z.string().min(1, "Title ist required").max(255),
     // description: z.string().min(1)
-    description: z.string().min(1, "Description is required")
+    description: z.string().min(1, "Description is required").max(65535),
+})
+
+export const patchIssueSchema = z.object({
+    title: z
+        .string()
+        .min(1, "Title ist required")
+        .max(255)
+        .optional(),
+    description: z
+        .string()
+        .min(1, "Description is required")
+        .max(65535)
+        .optional(),
+    assignedToUserId: z
+        .string()
+        .min(1, "AssignedToUserId is required.")
+        .max(255)
+        .optional()
+        .nullable()
 })
