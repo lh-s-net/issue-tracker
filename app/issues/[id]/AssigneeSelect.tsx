5558d80384f3cd461d267e6579ef656906219f21
JW <info@lh-s.net>
2023-11-05 13:34:48 +0100

    fetching data with react query

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -4,17 +4,30 @@ import React, {useEffect, useState} from 'react';
 import {Select} from "@radix-ui/themes";
 import axios from "axios";
 import {User} from "next-auth";
+import {useQuery} from "@tanstack/react-query";
+import {Skeleton} from "@/app/components";
 
 const AssigneeSelect = () => {
-    const [users, setUsers] = useState<User[]>([])
 
-    useEffect(() => {
-        const fetchUser = async () => {
-            const {data} = await axios.get<User[]>("/api/users")
-            setUsers(data)
-        }
-        fetchUser()
-    }, [])
+    const {data: users, error, isLoading} = useQuery<User[]>({
+        queryKey: ["users"],
+        queryFn: () => axios.get("/api/users").then(res => res.data),
+        staleTime: 60 * 1000, // 60 seconds cache - is optional
+        retry: 3
+    })
+
+    if (isLoading) return <Skeleton/>
+    if (error) return null
+
+    // eplaced with TQuery
+    // const [users, setUsers] = useState<User[]>([])
+    // useEffect(() => {
+    //     //     const fetchUser = async () => {
+    //     //         const {data} = await axios.get<User[]>("/api/users")
+    //     //         setUsers(data)
+    //     //     }
+    //     //     fetchUser()
+    //     // }, [])
 
     return (
         <Select.Root>
@@ -22,7 +35,7 @@ const AssigneeSelect = () => {
             <Select.Content>
                 <Select.Group>
                     <Select.Label>Suggestions</Select.Label>
-                    {users.map((user) =>
+                    {users?.map((user) =>
                         <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>
                     )}
 
