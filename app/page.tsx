23c44a5c5faf68d0f39c911f60690e02578959e3
JW <info@lh-s.net>
2023-11-09 23:08:07 +0100

    build the latestissue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import Pagination from "@/app/components/Pagination";
+import LatestIssues from "@/app/LatestIssues";
 
 export default function Home({searchParams}: { searchParams: { page: string } }) {
     return (
-        <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)}/>
+        <>
+            <LatestIssues/>
+        </>
     )
 }
