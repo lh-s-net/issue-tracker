a6d6eb7f087604e9164c3b596a31d8d492cd2444
JW <info@lh-s.net>
2023-11-09 23:34:54 +0100

    build the issue summery component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import Pagination from "@/app/components/Pagination";
 import LatestIssues from "@/app/LatestIssues";
+import IssueSummary from "@/app/IssueSummary";
+import prisma from "@/prisma/client";
 
-export default function Home({searchParams}: { searchParams: { page: string } }) {
+export default async function Home({searchParams}: { searchParams: { page: string } }) {
+
+    const open = await prisma.issue.count({where: {status: "OPEN"}})
+    const inProgress = await prisma.issue.count({where: {status: "IN_PROGRESS"}})
+    const closed = await prisma.issue.count({where: {status: "CLOSED"}})
+
     return (
         <>
+            <IssueSummary open={open} inProgress={inProgress} closed={closed}/>
             <LatestIssues/>
         </>
     )
