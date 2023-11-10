8d637496f1cc338ecacfca8fdc07a55ed946fa99
JW <info@lh-s.net>
2023-11-10 10:47:59 +0100

    build the issue chart component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import Pagination from "@/app/components/Pagination";
 import LatestIssues from "@/app/LatestIssues";
 import IssueSummary from "@/app/IssueSummary";
 import prisma from "@/prisma/client";
+import IssueChart from "@/app/IssueChart";
 
 export default async function Home({searchParams}: { searchParams: { page: string } }) {
 
@@ -12,6 +12,7 @@ export default async function Home({searchParams}: { searchParams: { page: strin
     return (
         <>
             <IssueSummary open={open} inProgress={inProgress} closed={closed}/>
+            <IssueChart open={open} inProgress={inProgress} closed={closed}/>
             <LatestIssues/>
         </>
     )
