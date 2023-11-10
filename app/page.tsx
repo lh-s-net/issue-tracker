03346a66d894a242d24a7d8cbf8b58b4fa2f8879
JW <info@lh-s.net>
2023-11-10 10:56:35 +0100

    layout the dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import prisma from "@/prisma/client";
 import IssueChart from "@/app/IssueChart";
 import {Flex, Grid} from "@radix-ui/themes";
 
-export default async function Home({searchParams}: { searchParams: { page: string } }) {
+export default async function Home() {
 
     const open = await prisma.issue.count({where: {status: "OPEN"}})
     const inProgress = await prisma.issue.count({where: {status: "IN_PROGRESS"}})
